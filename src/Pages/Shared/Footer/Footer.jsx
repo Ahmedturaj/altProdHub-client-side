582f516ac99c5f5335a2ceca6bd6f62d0fdefc95
Ahmedturaj <ahmedturaj12@gmail.com>
2024-05-14 16:18:24 +0600

    modify footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
                     <div>
                         <h3 className="text-gray-700 uppercase dark:text-white">Contact</h3>
                         <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+1 526 654 8965</span>
-                        <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">example@email.com</span>
+                        <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">turaj@email.com</span>
                     </div>
                 </div>
             </div>
@@ -73,7 +73,7 @@ const Footer = () => {
         <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700"/>
 
         <div>
-            <p className="text-center text-gray-500 dark:text-gray-400">© Brand 2023 - All rights reserved</p>
+            <p className="text-center text-gray-500 dark:text-gray-400">© AltProdHub 2023 - All rights reserved</p>
         </div>
     </div>
 </section>
